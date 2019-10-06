f4d6be83f19c8f9dc791735b0d0cd8c4609525d7
Ibra Lin <Ibra Lin>
2019-10-06 13:42:28 +0300

    added filteredimage path
    tested with local server using postman

--- src/server.ts
+++ src/server.ts
@@ -29,6 +29,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.get( "/filteredimage", async (req, res) => {
+
+    let { image_url } = req.query;
+
+    if (!image_url) {
+      return res.status(422).send({ error: 'image_url is required!' });
+    }
+
+    let filteredpath = await filterImageFromURL(image_url);
+    res.status(200).sendFile(filteredpath, () => { 
+      deleteLocalFiles([filteredpath]); 
+    });
+
+  });
+
+
   //! END @TODO1
   
   // Root Endpoint
