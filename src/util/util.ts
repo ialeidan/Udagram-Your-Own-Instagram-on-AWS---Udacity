dd10d68182475aa6980cf14b6a7de10d0c2c8e3e
Ibra Lin <Ibra Lin>
2019-10-06 14:47:58 +0300

    bugfix

--- src/util/util.ts
+++ src/util/util.ts
@@ -10,13 +10,13 @@ import Jimp = require('jimp');
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string>{
     return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
+        const photo = await Jimp.default.read(inputURL);
         const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
         await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
+        .write(__dirname+outpath, (img: any)=>{
             resolve(__dirname+outpath);
         });
     });
